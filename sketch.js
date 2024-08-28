a524e614fff9cc9349cbdfdf326343403129658c
tteokbokkiyummy <yseok1@lsu.edu>
2024-08-28 15:18:30 -0500

    ghjghgh

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 function setup() {
-    createCanvas(100,100);
+    createCanvas(windowWidth,windowHeight);
     background(150);
-    ellipse (width, height, 50, 50);
+    fill(255,0,0);
+    noStroke();
+    ellipse (width/2, height/2, 50, 50);
 }
 
 function draw() {
